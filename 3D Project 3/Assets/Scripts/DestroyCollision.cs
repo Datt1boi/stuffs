062c9d5a9290a91db4f419f4a12b00101da88d6e
Kyan0ver9000 <115186321+Kyan0ver9000@users.noreply.github.com>
2023-05-03 12:27:31 -0700

    the enemieds die now

---
+++ 3D Project 3/Assets/Scripts/DestroyCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using JetBrains.Annotations;
+
+public class DestroyCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "GUN") {
+			Destroy (collision.gameObject);
+		}
+	}
+}
