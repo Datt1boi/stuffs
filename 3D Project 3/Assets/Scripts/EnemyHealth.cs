ba6335db29e3d9c6917d882f09054b5f8d75dae3
Martin Garcia <115186046+Datt1boi@users.noreply.github.com>
2023-05-09 13:33:53 -0700

    oop

--- 3D Project 3/Assets/Scripts/EnemyHealth.cs
+++ 3D Project 3/Assets/Scripts/EnemyHealth.cs
@@ -4,39 +4,8 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public int maxHealth = 100;
-    private int currentHealth;
-    public string damageTag = "GUN";  // tag of the game object that can damage the enemy
-
-    private void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        currentHealth = maxHealth;
-        GetComponent<BoxCollider>().isTrigger = true;
-    }
-
-    private void OnCollision3D(BoxCollider collision)
-    {
-        if (collision.gameObject.tag == damageTag)
-        {
-            TakeDamage(1); // Deal 1 damage to the enemy
-            Destroy(collision.gameObject);
-        }
-    }
-
-    public void TakeDamage(int damageAmount)
-    {
-        currentHealth -= damageAmount;
-
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
-
-    private void Die()
-    {
-        // Handle enemy death logic here
         Destroy(gameObject);
     }
 }
-
