dea5aa028fc9e45c0745e44ab15bc6ec01954dcc
Martin Garcia <115186046+Datt1boi@users.noreply.github.com>
2023-05-09 13:57:17 -0700

    kys

--- 3D Project 3/Assets/Scripts/DestroyCollision.cs
+++ 3D Project 3/Assets/Scripts/DestroyCollision.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 using System.Collections;
-using JetBrains.Annotations;
 
 
 public class DestroyCollision : MonoBehaviour
 {
-    public int enemiesToKill = 10; // set the number of enemies to kill to move to the next scene
-    private int enemiesKilled = 0; // track the number of enemies killed
     public string enemyTag = "Enemy"; // set the tag for the enemy game object
 
     // Use this for initialization
@@ -16,22 +13,10 @@ public class DestroyCollision : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // check if the required number of enemies have been killed
-        if (enemiesKilled >= enemiesToKill)
-        {
-            // load the next scene
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
-
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (gameObject.tag == enemyTag)
         {
-            enemiesKilled++; // increment the enemies killed counter
             Destroy(gameObject);
         }
     }
